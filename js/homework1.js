7ba6ef437e5ab6e70a2c4ee66f49190ed0317079
Andriy <skysniper29@gmail.com>
2021-11-24 20:21:41 +0200

    first commit

---
+++ js/homework1.js
@@ -0,0 +1,46 @@
+// Task 2
+console.log("Ryzhak"); 
+
+
+
+// Task 3
+let string = "lesson";
+const namber = 1;
+
+alert(string);
+string = "JavaScript";
+alert(string);
+
+// Task 4 
+let str = "name";
+console.log(str);
+
+let number = 22;
+console.log(number);
+
+//boolean
+console.log(3*3 == 9);
+
+let age;
+console.log(age);
+
+let something = null;
+console.log(something);
+
+// or
+
+const obj = {
+    str: "name",
+    number: 22,
+    boolean: false
+};
+console.log(obj);
+
+// Task 5
+const isAdult = confirm("Вам є 18?");
+console.log(isAdult);
+
+// Task 6
+
+
+
