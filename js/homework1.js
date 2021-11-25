ab225bda490ef613d9a7a2281577559d4822d9ec
Andriy <skysniper29@gmail.com>
2021-11-25 22:24:54 +0200

    second commit

--- js/homework1.js
+++ js/homework1.js
@@ -1,46 +1,74 @@
-// Task 2
-console.log("Ryzhak"); 
+"use strict";
 
+//Task 2
+console.log("Ryzhak"); 
 
 
 // Task 3
 let string = "lesson";
 const namber = 1;
-
 alert(string);
 string = "JavaScript";
 alert(string);
 
+
 // Task 4 
 let str = "name";
-console.log(str);
+console.log( typeof str);
 
 let number = 22;
-console.log(number);
+console.log(typeof number);
 
 //boolean
 console.log(3*3 == 9);
 
 let age;
-console.log(age);
+console.log(typeof age);
 
 let something = null;
-console.log(something);
+console.log(typeof something);
 
-// or
-
-const obj = {
-    str: "name",
-    number: 22,
-    boolean: false
-};
-console.log(obj);
 
 // Task 5
+
 const isAdult = confirm("Вам є 18?");
 console.log(isAdult);
 
 // Task 6
 
+let Name = "Andriy";
+let surname = "Ryzhak";
+let groop = "654 JS Core";
+let birthday = 1998;
+const isMarried = false;
+console.log(typeof birthday);
+console.log(typeof isMarried);
+console.log(typeof Name);
+
+let age = 22;
+age = null;
+console.log(age);
+
+let und;
+console.log(und);
+
+//Task 7
+
+let login = prompt("Your login", "");
+let password = prompt("Your password","");
+let email = prompt("Your email","");
+
+alert(`Dear ${login}, your email is ${email}, your password is ${password}`);
+
+//Task 8
+
+let Hour = prompt("Введіть кількість годин","");
+alert(`В цій кількості годин ${Hour * 60 * 60} секунд`);
+
+let day = prompt("Введіть кількість днів","");
+alert(`В цій кількості днів ${day * 60 * 60 * 24} секунд`);
+
+let months = prompt("Введіть кількість місяців","");
+alert(`В цій кількості місяців ${months * 60 * 60 * 24 * 30} секунд`);
 
 
