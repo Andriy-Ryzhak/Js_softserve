309119f573703bdb7b2c311d077e37671064f608
Andriy <skysniper29@gmail.com>
2021-12-01 19:09:34 +0200

    first commit

--- js/homework2.js
+++ js/homework2.js
@@ -108,40 +108,20 @@ if (hour >= 23 && hour <= 24 || hour >= 0 && hour <= 5) {
 
 let hour = new Date().getHours();
 
-switch (hour) {
-  case 1:
-  case 2:
-  case 3:
-  case 4:
-  case 5:
-  case 23:
-    alert( "Доброго ночі" );
-    break;
+switch (true) {
+    case hour >= 23 && hour <= 24 || hour >= 0 && hour <= 5:
+        alert('Доброї ночі');
+        break;
+    case hour >= 6 && hour <= 11:
+        alert('Доброго ранку');
+        break;
+    case hour >= 12 && hour <= 17:
+        alert('Доброго дня');
+        break;
+    case hour >= 18 && hour <= 22:
+        alert('Доброго вечора');
+        break;
+}
 
-  case 6:
-  case 7:    
-  case 8:
-  case 9:
-  case 10:
-  case 11:
-    alert( "Доброго ранку" );
-    break;
 
-  case 12:
-  case 13:    
-  case 14:
-  case 15:
-  case 16:
-  case 17:
-    alert( "Доброго дня" );
-    break;
-
-  case 18:
-  case 19:    
-  case 20:
-  case 21:
-  case 23:
-    alert( "Доброї вечора" );
-    break;
-}  
 
