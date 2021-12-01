da3179a5d3738f6fbd19a4601173d61d321cdf29
Andriy <skysniper29@gmail.com>
2021-12-01 19:00:00 +0200

    first commit

--- js/homework2.js
+++ js/homework2.js
@@ -105,3 +105,43 @@ if (hour >= 23 && hour <= 24 || hour >= 0 && hour <= 5) {
     alert("Доброї вечора");
 }
     
+
+let hour = new Date().getHours();
+
+switch (hour) {
+  case 1:
+  case 2:
+  case 3:
+  case 4:
+  case 5:
+  case 23:
+    alert( "Доброго ночі" );
+    break;
+
+  case 6:
+  case 7:    
+  case 8:
+  case 9:
+  case 10:
+  case 11:
+    alert( "Доброго ранку" );
+    break;
+
+  case 12:
+  case 13:    
+  case 14:
+  case 15:
+  case 16:
+  case 17:
+    alert( "Доброго дня" );
+    break;
+
+  case 18:
+  case 19:    
+  case 20:
+  case 21:
+  case 23:
+    alert( "Доброї вечора" );
+    break;
+}  
+
