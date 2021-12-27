99c10ab1c94b34cf83aa5e9569c3be84bd5a3622
Andriy <skysniper29@gmail.com>
2021-12-27 12:23:26 +0200

    first commit

--- js/homework9.js
+++ js/homework9.js
@@ -1,1 +1,45 @@
 
+
+// Task 1
+
+function addTag() {
+    if ($("a").attr("href") === "https://") {
+        $('a').attr('target', '_blank');
+    }
+}
+addTag();
+
+// Task 2
+
+function changeStyle() {
+    let $allElements = $("h2.head");
+    $allElements.css('background-color', 'green');
+    $allElements.find(".inner").css("font-size", "35px");
+}
+
+changeStyle();
+
+// Task 3
+function changeLocation() {
+    const $allDivs = $("h3").next();
+
+    $allDivs.each(function () {
+        const h3Element = $(this).prev();
+        $(this).insertBefore(h3Element);
+    });
+}
+
+changeLocation();
+
+// Task 4
+
+$('.inputs input:checkbox').click(() => { 
+    let lengthEl = $('.inputs input:checkbox:checked').length;
+    let checkboxCheked = $('.inputs input:checkbox:checked');
+    if (lengthEl >= 3 ) {
+        if (checkboxCheked !== true) {
+            $('.inputs input:checkbox').not('.inputs input:checked').remove();
+        }
+            
+    }  
+});
